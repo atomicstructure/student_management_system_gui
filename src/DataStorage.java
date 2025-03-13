411ceb90584674b3382a894a79973a552ced7929
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-03-13 18:46:45 +0100

    Created the DataStorage class to simulate a database to store student and course data.

--- src/DataStorage.java
+++ src/DataStorage.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataStorage {
+    private List<Student> students;
+    private List<Course> courses;
+
+    public DataStorage() {
+        students = new ArrayList<>();
+        courses = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
 }
