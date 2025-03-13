c636a2c189b7c5a2046bfe63c0f64399c1300882
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-03-13 21:31:26 +0100

    Email and Major of the student has been added to the Student class as a private variable

--- src/Student.java
+++ src/Student.java
@@ -4,11 +4,15 @@ import java.util.Map;
 public class Student {
     private String studentId;
     private String name;
+    private String email;
+    private String major;
     private Map<Course, String> enrolledCourses; // Course -> Grade
 
-    public Student(String studentId, String name) {
+    public Student(String studentId, String name, String email, String major) {
         this.studentId = studentId;
         this.name = name;
+        this.email = email;
+        this.major = major;
         this.enrolledCourses = new HashMap<>();
     }
 
@@ -20,8 +24,24 @@ public class Student {
         return name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMajor(String Major) {
+        this.major = Major;
     }
 
     public void enrollCourse(Course course) {
@@ -40,6 +60,6 @@ public class Student {
 
     @Override
     public String toString() {
-        return "ID: " + studentId + ", Name: " + name;
+        return "ID: " + studentId + ", Name: " + name + ", Email: " + email + ", Major: " + major;
     }
 }
